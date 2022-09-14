12208bc70c21136f07cf7be511be6dfd295417fa
Farhannovaal <Farhannovaal31@gmail.com>
2022-09-14 06:41:29 -0700

    Gallery image

--- edit.js
+++ edit.js
@@ -21,29 +21,25 @@ let words = '';
     }
 
     setTimeout(Myjob,300);
+
+    return;
 })();
 
 
-// var card = `<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-// <div class="carousel-inner">
-//   <div class="carousel-item active" data-bs-interval="10000">
-//     <img src="..." class="d-block w-100" alt="...">
-//   </div>
-//   <div class="carousel-item" data-bs-interval="2000">
-//     <img src="..." class="d-block w-100" alt="...">
-//   </div>
-//   <div class="carousel-item">
-//     <img src="..." class="d-block w-100" alt="...">
-//   </div>
-// </div>
-// <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-//   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-//   <span class="visually-hidden">Previous</span>
-// </button>
-// <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-//   <span class="carousel-control-next-icon" aria-hidden="true"></span>
-//   <span class="visually-hidden">Next</span>
-// </button>
-// </div>`;
 
-// if()
+const container = document.querySelector('.container-Gambar');
+const jumbo = document.querySelector('.jumbo');
+
+container.addEventListener('click', function(e){
+
+    //cek yang diklik 
+
+    if (e.target.className == 'thumb'){
+
+        jumbo.src = e.target.src;
+
+    }
+
+
+
+});
