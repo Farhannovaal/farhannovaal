8f1726cf4580b19afab954566d466a25919bc70a
Farhan Noval Hidayat <farhannovaal31@gmail.com>
2023-08-17 18:13:59 +0700

    Update

--- js/edit.js
+++ js/edit.js
@@ -1,70 +1,62 @@
+var rocket = document.querySelector('#rocket');
+var manJump = document.querySelector('#man-jump');
+var moon = document.querySelector('#Moon');
 
-	const rocket = document.querySelector('#rocket');
-	const manJump = document.querySelector('#man-jump');
-	const moon = document.querySelector('#Moon');  
-	function animate() {
-	  Velocity(rocket, {
-		top: '50px',
-		// left: '40%',
+function animate() {
+	Velocity(rocket, {
+		top: '20px',
 		rotateZ: '5deg'
-	  }, {
+	}, {
 		duration: 7000
-	  }),
+	});
 
-	  Velocity(rocket,{
-		// left:'40%',
-		rotateZ:'15deg'
-	  },{
-		duration:7000
-	  }),
-	  Velocity(rocket,{
-		left:'40%',
-		rotateZ:'30deg'
-	  },{
-		duration:7000
-	  }),
+	Velocity(rocket, {
+		rotateZ: '15deg'
+	}, {
+		duration: 7000
+	});
 
+	Velocity(rocket, {
+		left: '30%',
+		rotateZ: '0deg'
+	}, {
+		duration: 7000
+	});
 
-	  Velocity(manJump,{
-		opacity:1
-	  },{
-		delay:13000,
-		duration:1000
-	  }),
-	   Velocity(manJump,{
-		top:400,
-	  },{
-		queue:false,
-		delay:17000,
-		duration:12000
-	  }),
+	Velocity(manJump, {
+		opacity: 1
+	}, {
+		delay: 13000,
+		duration: 1000
+	});
 
-	  Velocity(moon,{
-		left:'35%'
-	  },{
-		duration:12000
-	  })
-	}
-  
-	animate();
-  
-	console.log('Velocity function called');
+	Velocity(manJump, {
+		top: '310px'
+	}, {
+		queue: false,
+		delay: 17000,
+		duration: 12000
+	});
 
-  
+	Velocity(moon, {
+		left: '25%'
+	}, {
+		duration: 12000
+	});
+}
 
-	const btnPublish = document.querySelector('.publish-btn.news');
-	const cardPopUp = document.querySelector('.card-popup');
-	const closePop = document.querySelector('.close');
+animate();
 
-      btnPublish.addEventListener('click',function(){
-		cardPopUp.classList.add('active');
-	  })
+console.log('Velocity function called');
 
-      closePop.addEventListener('click', function() {
-        cardPopUp.classList.remove('active');
-      });
+const btnPublish = document.querySelector('.publish-btn.news');
+const cardPopUp = document.querySelector('.card-popup');
+const closePop = document.querySelector('.close');
 
+btnPublish.addEventListener('click', function () {
+	cardPopUp.classList.add('active');
+});
 
-	  
-
-    
+closePop.addEventListener('click', function () {
+	cardPopUp.classList.remove('active');
+});
